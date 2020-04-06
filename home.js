f1ec4ae0dc10d1290e82a7b2103eacd1007ec006
Amr Salah <Amrsalahshahin@gmail.com>
2020-04-06 14:12:23 +0200

    ghvh

---
+++ home.js
@@ -0,0 +1,9 @@
+document.getElementById('sign-in').addEventListener('click', function(){
+    document.getElementsByClassName('lastform')[0].style.display = 'none';
+    
+    document.getElementsByClassName('form')[0].style.display = 'block';
+    
+})
+
+
+console.log('Hello');
