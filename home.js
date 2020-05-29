825ad2fca698e594b288207374dc6fc137f460a9
HimaTaha <ibrahim.taha@modeso.ch>
2020-05-29 18:40:28 +0200

    adding comments for further improvments

--- home.js
+++ home.js
@@ -1,27 +1,33 @@
 
-const signUpForm = document.getElementById('signUpForm');
-const nameField = document.getElementById('name');
-let passwordField = document.getElementById('password');
-const signButton = document.getElementById('sign-up');
 
-const names = ['amr','hima'];
-const passwords = ['12345678','910111213'];
+const signUp = document.getElementById('singUpButton')
+signUp.addEventListener('click', function () {
+    document.getElementById('signInForm').style.display = 'none';
+    document.getElementById('signUpForm').style.display = 'block';
+})
+const signUpForm = document.getElementById('signUpForm'); // FIXME- if this element used ? if not delete it
+const signButton = document.getElementById('sign-up'); // FIXME- if this element used ? if not delete it
+
+const names = ['amr', 'hima'];
+const passwords = ['12345678', '910111213'];
 
 
-function Check(){
+function Check() {
+    const nameField = document.getElementById('name');
+    let passwordField = document.getElementById('password');
     let error;
-    for (let i = 0; i < names.length; i++){
-        if (nameField.value !== names[i]  || passwordField.value !== passwords[i]){
+    for (let i = 0; i < names.length; i++) {
+        if (nameField.value !== names[i] || passwordField.value !== passwords[i]) {
             error = 'Please Check UserName or Password';
             document.getElementById('error_para').innerHTML = error;
             console.log(nameField.value);
             console.log(names[i]);
             console.log(passwordField.value);
             console.log(passwords[i]);
-            return false;
+            return false; // FIXME- try to login with hima and 910111213 and solve it
 
-        } 
-        else{
+        }
+        else {
             error = 'Good JOb';
             document.getElementById('error_para').innerHTML = error;
             console.log(nameField.value);
@@ -30,35 +36,32 @@ function Check(){
             console.log(passwords[i]);
             return false;
         }
-    
-    }   
+
+    }
 }
 
-const signUp = document.getElementById('singUpButton')
-signUp.addEventListener('click',function(){
-    document.getElementById('signInForm').style.display = 'none';
-    document.getElementById('signUpForm').style.display = 'block';
-})
-
-const firstName = document.getElementById('first-Name');
-const lastName = document.getElementById('last-Name');
-const Email = document.getElementById('Email');
-const Password = document.getElementById('Pass');
-const Address = document.getElementById('Address');
-const message = document.getElementById('message');
-const Done = document.getElementById('done')
 
 
-function finish () {
-    if (firstName.value == '' || lastName.value == '') { message.innerHTML = ('Please Enter Your Name'); return false;}
-    else if (Email.value = '' || Email.value.search('@') == -1 ) { message.innerHTML = ('Please Enter a Valid Mail');return false;}
-    else if (Password.value.length < 8) {message.innerHTML = ('Password Must be at least 8 digits'); return false;}
-    else if (Address.value == '') { message.innerHTML = ('Please Enter Your Address'); return false;}
-    else {Done.addEventListener('click',function(){
+
+function finish() {
+    // this variables should be inside the function only
+    const firstName = document.getElementById('first-Name');
+    const lastName = document.getElementById('last-Name');
+    const Email = document.getElementById('Email');
+    const Password = document.getElementById('Pass');
+    const Address = document.getElementById('Address');
+    const message = document.getElementById('message');
+    const Done = document.getElementById('done')
+    if (firstName.value == '' || lastName.value == '') { message.innerHTML = ('Please Enter Your Name'); return false; } // FIXME: why do you always return false?
+    else if (Email.value = '' || Email.value.search('@') == -1) { message.innerHTML = ('Please Enter a Valid Mail'); return false; }
+    else if (Password.value.length < 8) { message.innerHTML = ('Password Must be at least 8 digits'); return false; }
+    else if (Address.value == '') { message.innerHTML = ('Please Enter Your Address'); return false; }
+    else {
+        Done.addEventListener('click', function () {
             document.getElementById('signInForm').style.display = 'block';
             document.getElementById('signUpForm').style.display = 'none';
-    })
-    } ;return false;
+        })
+    }; return false;
 }
 
 
